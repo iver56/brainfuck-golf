9c9b5bb7c4ad120c65031684c7306cbd3635daba
iver56 <iver56@hotmail.com>
2014-06-07 19:00:20 +0200

    brainfuck 619

--- brainfuck.py
+++ brainfuck.py
@@ -3,22 +3,22 @@ m = sys.stdin.readline().split(" ")
 p = m[0]
 s = list(m[1]) if len(m) > 1 else []
 dp = 0
-d = [0]*30000
 pp = 0
-bracket_stack = []
+d=[]
 brackets = {}
 for i in range(len(p)):
-    if p[i] == '[': bracket_stack.append(i)
-    if p[i] == ']': brackets[bracket_stack.pop()] = i
+    if p[i] == '[': d.append(i)
+    if p[i] == ']': brackets[d.pop()] = i
 for k in brackets.keys(): brackets[brackets[k]] = k
+d=[0]*30000
 while pp < len(p):
     c = p[pp]
-    if c == '>': dp += 1
-    elif c == '<': dp -= 1
-    elif c == '+': d[dp] += 1
-    elif c == '-': d[dp] -= 1
-    elif c == '.': print chr(d[dp])
-    elif c == ',': d[dp] = ord(s.pop(0)) if s else -1
-    elif c == '[' and not d[dp]: pp = brackets[pp]
-    elif c == ']': pp = brackets[pp] - 1
-    pp += 1
+    if c == '>': dp+=1
+    if c == '<': dp-=1
+    if c == '+': d[dp]+=1
+    if c == '-': d[dp]-=1
+    if c == '.': print chr(d[dp])
+    if c == ',': d[dp]=ord(s.pop(0)) if s else -1
+    if c == '[' and not d[dp]: pp=brackets[pp]
+    if c == ']': pp=brackets[pp]-1
+    pp+=1
