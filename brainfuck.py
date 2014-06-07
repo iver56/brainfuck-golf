82c8b021541374397e3e58446a771c2243787b9e
iver56 <iver56@hotmail.com>
2014-06-07 18:47:26 +0200

    brainfuck 981

--- brainfuck.py
+++ brainfuck.py
@@ -2,9 +2,8 @@ import sys
 
 myinput = sys.stdin.readline().split(" ")
 program = myinput[0]
-stdin = myinput[1] if len(myinput) > 1 else ""
+stdin = list(myinput[1]) if len(myinput) > 1 else []
 
-stdin_pointer = 0
 data_pointer = 0
 data = [0] * 30000
 program_pointer = 0
@@ -12,32 +11,20 @@ program_pointer = 0
 bracket_stack = []
 brackets = {}
 for i in range(len(program)):
-    if program[i] == '[':
-        bracket_stack.append(i)
-    if program[i] == ']':
-        brackets[bracket_stack.pop()] = i
+    if program[i] == '[': bracket_stack.append(i)
+    if program[i] == ']': brackets[bracket_stack.pop()] = i
 for k in brackets.keys():
     brackets[brackets[k]] = k
 
 while program_pointer < len(program):
     c = program[program_pointer]
-    if c == '>':
-        data_pointer += 1
-    elif c == '<':
-        data_pointer -= 1
-    elif c == '+':
-        data[data_pointer] += 1
-    elif c == '-':
-        data[data_pointer] -= 1
-    elif c == '.':
-        print chr(data[data_pointer])
-    elif c == ',':
-        data[data_pointer] = ord(stdin[stdin_pointer]) if stdin_pointer < len(stdin) else -1
-        stdin_pointer += 1
-    elif c == '[':
-        if not data[data_pointer]:
-            program_pointer = brackets[program_pointer]
-    elif c == ']':
-        program_pointer = brackets[program_pointer] - 1
+    if c == '>': data_pointer += 1
+    elif c == '<': data_pointer -= 1
+    elif c == '+': data[data_pointer] += 1
+    elif c == '-': data[data_pointer] -= 1
+    elif c == '.': print chr(data[data_pointer])
+    elif c == ',': data[data_pointer] = ord(stdin.pop()) if stdin else -1
+    elif c == '[' and not data[data_pointer]: program_pointer = brackets[program_pointer]
+    elif c == ']': program_pointer = brackets[program_pointer] - 1
     program_pointer += 1
 
