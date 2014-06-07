42c32281b327f8229c17ee59b63f98e83ef61863
iver56 <iver56@hotmail.com>
2014-06-07 20:15:24 +0200

    brainfuck 443

--- brainfuck.py
+++ brainfuck.py
@@ -1,9 +1,8 @@
 import sys
 m=sys.stdin.readline().split(" ")
 p=m[0]
-s=list(m[1]) if len(m) > 1 else []
-dp=0
-pp=0
+s=list(m[1]) if len(m)>1 else []
+x=y=0
 d=[]
 b={}
 for i in range(len(p)):
@@ -11,14 +10,14 @@ for i in range(len(p)):
 	if p[i] == ']':b[d.pop()]=i
 for k in b.keys():b[b[k]]=k
 d=[0]*30000
-while pp < len(p):
-	c = p[pp]
-	if c=='>':dp+=1
-	if c=='<':dp-=1
-	if c=='+':d[dp]+=1
-	if c=='-':d[dp]-=1
-	if c=='.':print chr(d[dp])
-	if c==',':d[dp]=ord(s.pop(0)) if s else -1
-	if c=='[' and not d[dp]:pp=b[pp]
-	if c==']':pp=b[pp]-1
-	pp+=1
+while y<len(p):
+	c=p[y]
+	if c=='>':x+=1
+	if c=='<':x-=1
+	if c=='+':d[x]+=1
+	if c=='-':d[x]-=1
+	if c=='.':print chr(d[x])
+	if c==',':d[x]=ord(s.pop(0)) if s else -1
+	if c=='[' and not d[x]:y=b[y]
+	if c==']':y=b[y]-1
+	y+=1
