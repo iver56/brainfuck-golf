e70b79062aabe2eb262c8243de17ed681b52d0b6
iver56 <iver56@hotmail.com>
2014-06-09 21:18:30 +0200

    brainfuck 372

--- brainfuck.py
+++ brainfuck.py
@@ -1,9 +1,9 @@
 import sys
-m=sys.stdin.readline().split()
+m=sys.stdin.read().split()
 p=m.pop(0)
 s=x=y=0
 if m:s=list(m[0])
-d=[0]*30000
+d=[0]*9**5
 b={}
 l=len(p)
 for i in range(l):
@@ -11,9 +11,7 @@ for i in range(l):
 	if p[i]==']':u=d.pop();b[u],b[i]=i,u
 while y<l:
 	c=p[y]
-	if c=='>':x+=1
-	if c=='<':x-=1
-	if c in '+-':d[x]+=int(c+'1')
+	x+=c=='>';x-=c=='<';d[x]+=c=='+';d[x]-=c=='-'
 	if c=='.':print chr(d[x])
 	if c==',':d[x]=ord(s.pop(0)) if s else -1
 	if (d[x]==0)*c=='[':y=b[y]
